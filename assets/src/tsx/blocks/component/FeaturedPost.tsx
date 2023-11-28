4c93ab75337d8bff637879cbc8562fc6622133f2
James <jamesglendenning1@gmail.com>
2023-11-28 18:58:29 +0000

    Can now set an override and fallback to the base image

---
+++ assets/src/tsx/blocks/component/FeaturedPost.tsx
@@ -0,0 +1,60 @@
+
+const { MediaUpload, MediaUploadCheck } = wp.blockEditor;
+const { Button } = wp.components;
+
+const FeaturedPost = ({image = '', title, attributes, setAttributes}) => {
+
+	const removeMedia = () => {
+		setAttributes({
+			mediaId: 0,
+			mediaUrl: ''
+		});
+	}
+
+ 	const onSelectMedia = (media) => {
+		setAttributes({
+			mediaId: media.id,
+			mediaUrl: media.url
+		});
+	}
+
+	return(
+		<>
+		<article className="stage">
+				<div className="feature-image">
+					<img src={attributes.mediaId ? attributes.mediaUrl : image}/>
+				</div>
+				<div className="headline">
+					<h2 className="heading">{title}</h2>
+				</div>
+			</article>
+
+			<MediaUploadCheck>
+				<MediaUpload
+					onSelect={onSelectMedia}
+					value={attributes.mediaId}
+					allowedTypes={ ['image'] }
+					render={({open}) => (
+						<Button
+							className="mcb-stage-override is-primary"
+							onClick={open}
+						>
+							<span className="dashicon dashicons dashicons-camera-alt"></span>
+						</Button>
+					)}
+				/>
+				{
+					attributes.mediaId ?
+					<Button
+						className="mcb-stage-remove mcb-stage-override is-danger"
+						onClick={removeMedia}
+					>
+						<span className="dashicon dashicons dashicons-trash"></span>
+					</Button>
+				: ''}
+			</MediaUploadCheck>
+		</>
+	)
+};
+
+export default FeaturedPost;
--- assets/src/tsx/blocks/main.tsx
+++ assets/src/tsx/blocks/main.tsx
@@ -1,7 +1,5 @@
 import ListView from "./listView";
-
-const { MediaUpload, MediaUploadCheck } = wp.blockEditor;
-const { Button } = wp.components;
+import FeaturedPost from "./component/FeaturedPost";
 
 const MainStage = ({articles, featuredImage, attributes, setAttributes}) => {
 
@@ -9,59 +7,15 @@ const MainStage = ({articles, featuredImage, attributes, setAttributes}) => {
 	const article     = articles[0];
 	const articleList = articles.slice(1);
 
-	const removeMedia = () => {
-		setAttributes({
-			mediaId: 0,
-			mediaUrl: ''
-		});
-	}
- 
- 	const onSelectMedia = (media) => {
-		setAttributes({
-			mediaId: media.id,
-			mediaUrl: media.url
-		});
-	}
- 
-	const blockStyle = {
-		backgroundImage: attributes.mediaUrl != '' ? 'url("' + attributes.mediaUrl + '")' : 'none'
-	};
-
 	return (
 		<div className="mcb-main-stage">
-			<article className="stage">
-				<div className="feature-image">
-					<img src={attributes.mediaUrl ?? article.thumnail}/>
-				</div>
-				<div className="headline">
-					<h2 className="heading">{article.title}</h2>
-				</div>
-			</article>
 
-			<MediaUploadCheck>
-				<MediaUpload
-					onSelect={onSelectMedia}
-					value={attributes.mediaId}
-					allowedTypes={ ['image'] }
-					render={({open}) => (
-						<Button
-							className="mcb-stage-override is-primary"
-							onClick={open}
-						>
-							<span className="dashicon dashicons dashicons-camera-alt"></span>
-						</Button>
-					)}
-				/>
-				{
-					attributes.mediaId ?
-					<Button
-						className="mcb-stage-remove mcb-stage-override is-danger"
-						onClick={removeMedia}
-					>
-						<span className="dashicon dashicons dashicons-trash"></span>
-					</Button>
-				: ''}
-			</MediaUploadCheck>
+			<FeaturedPost
+				title={article.title}
+				image={article.thumbnail}
+				attributes={attributes}
+				setAttributes={setAttributes}
+			/>
 
 			<ListView articles={articleList} attributes={attributes} setAttributes={setAttributes}  />
 		</div>
